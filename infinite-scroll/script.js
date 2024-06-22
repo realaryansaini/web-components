545f410b810391c4ea75741f041a438b74964852
Aryan <itzaryansaini@gmail.com>
2024-06-22 21:07:03 +0530

    controling scroll with js

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,1 +1,15 @@
 
+const scrollor_containers = document.querySelectorAll(".scrollor-container")
+
+scrollor_containers.forEach((scroller_container)=>{
+    const scrollor = scroller_container.querySelector(".scrollor")
+    const spans =  Array.from(scrollor.children)
+    // console.log(spans);
+    spans.forEach((item) =>{
+        const duplicateItem = item.cloneNode(true)
+        // console.log(duplicateItem);
+        duplicateItem.setAttribute("aria-hidden",true)
+        scrollor.appendChild(duplicateItem)
+    })
+
+})
